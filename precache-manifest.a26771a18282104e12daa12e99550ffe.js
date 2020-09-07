b1044e14278aad023b76d1d8c748034779c44ca2
Siddhant <mamtajain81@gmail.com>
2020-09-07 10:15:28 +0530

    Updates

---
+++ precache-manifest.a26771a18282104e12daa12e99550ffe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afd1449ee2c75261bf1b2ec646e6ff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44e0282217929ac45c4e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0081a9f9b2daf4538c91",
+    "url": "/static/js/2.d8b6502a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8b6502a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44e0282217929ac45c4e",
+    "url": "/static/js/main.90c9774f.chunk.js"
+  },
+  {
+    "revision": "10e31650928e8e743aea",
+    "url": "/static/js/runtime-main.21462310.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
